8c8bfe4d72adf72e06ae378b1a21adfa583ce581
DanRoal <danroal@ciencias.unam.mx>
2026-05-04 01:15:31 -0600

    added unit test for textnode

---
+++ src/textnode.py
@@ -0,0 +1,23 @@
+from enum import Enum
+
+class TextType(Enum):
+    PLAIN = "text"
+    BOLD = "**Bold text**"
+    ITALIC = "_Italic text_"
+    CODE = "`Code text`"
+    LINK = "[anchor text](url)"
+    IMAGE = "![alt text](url)"
+
+class TextNode:
+    def __init__(self, text, text_type: TextType, url = None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, other):
+        if not isinstance(other, TextNode):
+            return False
+        return self.__dict__ == other.__dict__
+    
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
