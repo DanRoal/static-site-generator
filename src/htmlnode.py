f98c7dbd81bea28302e01b9e7cc902ffe3f4ebe2
DanRoal <danroal@ciencias.unam.mx>
2026-05-06 17:06:38 -0600

    ParentNode added wiwth some tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -4,17 +4,17 @@ class HTMLNode:
         self.value = value
         self.children = children
         self.props = props
-        
-
 
     def to_html(self):
         raise NotImplementedError
     
     def props_to_html(self):
         formated = ""
+        if self.props == None:
+            return formated
         for key, val in self.props.items():
             formated += f' {key}="{val}"'
-        return formated.strip()
+        return formated
     
     def __repr__(self):
         return f"HTMLNode: {self.__dict__}"
@@ -23,4 +23,31 @@ class HTMLNode:
         return self.__dict__ == other.__dict__
     
 class LeafNode(HTMLNode):
-    pass
+    def __init__(self, tag, value, props = None):
+        super().__init__(tag, value, None, props)
+        self.__dict__.pop("children")
+    
+    def to_html(self):
+        if self.value == None:
+            raise ValueError
+        if self.tag == None:
+            return f"{self.value}"
+        res = f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+        return res
+    
+    def __repr__(self):
+        return f"LeafNode: {self.__dict__}"
+    
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props = None):
+        super().__init__(tag, None, children, props)
+        self.__dict__.pop("value")
+    
+    def to_html(self):
+        if self.tag == None:
+            raise ValueError("No tag")
+        if self.children == None:
+            raise ValueError("No children")
+        branches = [i.to_html() for i in self.children]
+        res = f"<{self.tag}{self.props_to_html()}>{"".join(branches)}</{self.tag}>"
+        return res
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -35,7 +35,51 @@ class TestHTMLNode(unittest.TestCase):
         node3 = HTMLNode("a", "This is another link", None, test_prop2)
         self.assertEqual(node.props_to_html(), node2.props_to_html())
         self.assertNotEqual(node2.props_to_html(), node3.props_to_html())
+    
+class TestLeafNode(unittest.TestCase):
+    def test_eq(self):
+        test_props = {"href":"google.com"}
+        leaf1 = LeafNode("a", "Hello, world!", test_props)
+        leaf2 = LeafNode("a", "Hello, world!", test_props)
+        self.assertEqual(leaf1, leaf2)
 
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>") 
+
+class TestParentNode(unittest.TestCase):
+    def test_to_html_with_children(self):
+        child_node = LeafNode("span", "child")
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(parent_node.to_html(), "<div><span>child</span></div>")
+
+    def test_to_html_with_grandchildren(self):
+        grandchild_node = LeafNode("b", "grandchild")
+        child_node = ParentNode("span", [grandchild_node])
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(
+            parent_node.to_html(),
+            "<div><span><b>grandchild</b></span></div>",
+        )  
+
+    def test_to_html_with_several_children(self):
+        grandgrandchild_node = LeafNode("i", "grandgrandchild")
+
+        grandchild_node1 = LeafNode("b", "grandchild1")
+        grandchild_node2 = ParentNode("div", [grandgrandchild_node])
+        grandchild_node3 = LeafNode("b", "grandchild3")
+
+        child_node1 = ParentNode("span", [grandchild_node1])
+        child_node2 = ParentNode("span", [grandchild_node2])
+        child_node3 = ParentNode("span", [grandchild_node3])
+
+        parent_node = ParentNode("div", [child_node1, child_node2])
+
+        self.assertEqual(
+            parent_node.to_html(),
+            "<div><span><b>grandchild1</b></span><span><div><i>grandgrandchild</i></div></span></div>",
+        )  
 
         
 
