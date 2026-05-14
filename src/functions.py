46059170bcf162e58320fb63da94063cb30798d9
DanRoal <danroal@ciencias.unam.mx>
2026-05-13 21:36:37 -0600

    Added split_nodes_links and images, also some tests

--- src/functions.py
+++ src/functions.py
@@ -44,6 +44,50 @@ def split_nodes_delimiter(old_nodes:list[TextNode], delimiter:str, text_type:Tex
                 res.append(TextNode(text, text_type))
     return res
 
+def split_nodes_image(old_nodes:list[TextNode]):
+    res= []
+    for node in old_nodes:
+        if node.text_type is not TextType.TEXT:
+            res.append(node)
+            continue
+        images = extract_markdown_images(node.text)
+        splited = [node.text]
+        for alt, img_link in images:
+            splited.extend(splited.pop(-1).split(f"![{alt}]({img_link})",1))
+        index = 0
+        while index < len(images):
+            if splited[0] != "":
+                res.append(TextNode(splited[0], TextType.TEXT))
+            res.append(TextNode(images[index][0], TextType.IMAGE, images[index][1]))
+            splited.pop(0)
+            index+=1
+        for remain in splited:
+            if splited[0] != "":
+                res.append(TextNode(remain, TextType.TEXT))
+    return res
+
+def split_nodes_links(old_nodes:list[TextNode]):
+    res= []
+    for node in old_nodes:
+        if node.text_type is not TextType.TEXT:
+            res.append(node)
+            continue
+        links = extract_markdown_links(node.text)
+        splited = [node.text]
+        for alt, link in links:
+            splited.extend(splited.pop(-1).split(f"[{alt}]({link})",1))
+        index = 0
+        while index < len(links):
+            if splited[0] != "":
+                res.append(TextNode(splited[0], TextType.TEXT))
+            res.append(TextNode(links[index][0], TextType.LINK, links[index][1]))
+            splited.pop(0)
+            index+=1
+        for remain in splited:
+            if splited[0] != "":
+                res.append(TextNode(remain, TextType.TEXT))
+    return res
+
 def extract_markdown_images(text):
     images = re.findall(r"!\[(.*?)\]\((.*?)\)", text)
     return images
@@ -51,4 +95,4 @@ def extract_markdown_images(text):
 def extract_markdown_links(text):
     links = re.findall(r"(?<!!)\[(.*?)\]\((.*?)\)",text)
     return links
-        
+
--- src/test_functions.py
+++ src/test_functions.py
@@ -1,9 +1,9 @@
-from functions import split_nodes_delimiter, extract_markdown_images, extract_markdown_links
+from functions import split_nodes_delimiter, split_nodes_image, split_nodes_links, extract_markdown_images, extract_markdown_links
 from textnode import TextNode, TextType
 import unittest
 
-class TestTextNode(unittest.TestCase):
-    def test_code(self):
+class TestSplittingTextNode(unittest.TestCase):
+    def test_split_code(self):
         node = TextNode("This is text with a `code block` word", TextType.TEXT)
         code = [
             TextNode("This is text with a ", TextType.TEXT),
@@ -14,7 +14,7 @@ class TestTextNode(unittest.TestCase):
 
         self.assertEqual(new_nodes, code)
 
-    def test_bold(self):
+    def test_split_bold(self):
         node = TextNode("This is text with **bold text**", TextType.TEXT)
         bold = [
             TextNode("This is text with ", TextType.TEXT),
@@ -23,9 +23,8 @@ class TestTextNode(unittest.TestCase):
         new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
 
         self.assertEqual(new_nodes, bold)
-
         
-    def test_none(self):
+    def test_split_none(self):
 
         node = TextNode("This is text with no special word", TextType.TEXT)
         nospecial = [
@@ -39,6 +38,43 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is text with an _invalid syntax", TextType.TEXT)
 
         self.assertRaises(Exception, split_nodes_delimiter, [node], "_", TextType.ITALIC)
+
+    def test_split_images(self):
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.TEXT),
+                TextNode(
+                    "second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
+                ),
+            ],
+            new_nodes,
+        )
+
+    def test_split_links(self):
+        node = TextNode(
+            "This is text with a [link](https://i.imgur.com/zjjcJKZ.png) and another [second link](https://i.imgur.com/3elNhQu.png)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_links([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with a ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.TEXT),
+                TextNode(
+                    "second link", TextType.LINK, "https://i.imgur.com/3elNhQu.png"
+                ),
+            ],
+            new_nodes,
+        )
+
 
 class TestExtractMarkdown(unittest.TestCase):
     def test_extract_markdown_images(self):
